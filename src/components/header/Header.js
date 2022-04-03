ec7828aa9b5350150065b07ccb2d033adbba1aff
dwaipayan01 <dwaipayanjoy1998@gmail.com>
2022-04-03 15:16:33 +0600

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Shoes Shop</h1>
+            <h2>Chose your best shoes</h2>
+        </div>
+    );
+};
+
+export default Header;
